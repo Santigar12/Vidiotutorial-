ebfd30a4196416677c7868927ead0a576f231d39
Ara25cely <aracelyjacinto@gmail.com>
2023-09-17 16:50:53 -0600

    Agregando documento con la base de datos

---
+++ CreateDB.py
@@ -0,0 +1,38 @@
+import sqlite3 as sql
+
+def createDB():
+    conn = sql.connect("PrimerBaseDeDatos.db")
+    conn.commit()
+    conn.close()
+createDB()
+
+def createTable():
+    conn = sql.connect("PrimerBaseDeDatos.db")
+    cursor = conn.cursor()
+    cursor.execute(
+        """CREATE TABLE alumno(
+        name text,
+        edad integer
+        )"""
+    )
+    conn.commit()
+    conn.close()
+#createTable()
+
+
+def InsertRow(nombre,edad):
+
+    conn = sql.connect("PrimerBaseDeDatos.db")
+    cursor = conn.cursor()
+    
+    consulta = f"INSERT INTO alumno VALUES('{nombre}', '{edad}')"
+    cursor.execute(consulta)
+
+    conn.commit()
+    conn.close()
+
+
+
+
+
+  
